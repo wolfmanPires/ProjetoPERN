54c2f1285462a094c3303da2969f659644cd48e0
wolfmanPires <81977731+wolfmanPires@users.noreply.github.com>
2025-11-11 13:12:55 +0000

    Preparation of the middleware that will access the DB
    - Installed nodemon:
    >npm i nodemon -D
    - Imported and setted up ExpressJS, CORS, Helmet, Morgan and dotEnv to be the access and safety middlewares of the backend;
    - Configured the .env file to the desired port

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import helmet from "helmet";   //Helmet e um middleware de seguranca que facilita a configuracao de ferramentas de protecao da app atraves de headers HTTP
+import morgan from "morgan";   //Morgan perseva os pedidos de header HTTP num ficheiro log
+import cors from "cors";       //Permite suprimir os erros de CORS dos browsers
+import dotenv from "dotenv";   //Acesso as variaveis de ambiente do ficheiro .env
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.get("/", (req,res)=>{
+    res.send("Resposta do backend")
+})
+
+app.listen(PORT,()=>{
+    console.log("Servidor ExpressJS ativo na port "+PORT)
+})
