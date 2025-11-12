00332091c4db72e42af4e5f251780dcdea029200
wolfmanPires <81977731+wolfmanPires@users.noreply.github.com>
2025-11-12 23:49:46 +0000

    Finishing CRUD for the API on the routes and controller
    - Altered 'productController.js' and 'productRoutes.js' to include actions to update and delete items from the products table from the DB

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,12 @@
 import pool from "../config/db.js"
 
 //sistema de verificacao com erros (nao deixar DB ser corrompida)
+
+/////////
+/////GET
+/////////
+
+//receber todos os dados da tabela dos produtos
 export const getAllProducts = async (req,res) => {
     try{
         const RESULT = await pool.query("SELECT * FROM products");
@@ -12,12 +18,84 @@ export const getAllProducts = async (req,res) => {
     }
 }
 
-export const createProduct = async (req,res) => {
-    const {NAME,PRICE} = req.body;
+//receber um elemento especifico da tabela dos produtos pelo ID
+export const getProduct = async (req,res) => {
+    const {ID} = req.params;
 
     try{
-        const RESULT = await pool.query("INSERT INTO products (name, price) VALUES ($1,$2) RETURNING *", [NAME,PRICE]);
-        res.status(201).json(RESULT.rows[0]);
+        if(!ID){
+            res.status(400).json({message: "ID não existente, por favor tente outro"})
+        }else{
+            const RESULT = await pool.query("SELECT * FROM products WHERE id=$1", [ID]);
+            res.status(200).json(RESULT.rows);
+        }
+    }catch (err){
+        console.error("Erro detetado no getAllProducts do productController.js: ",err);
+        //Enviar codigo de erro interno de servidor 
+        res.status(500).json({message: "Erro detetado no getAllProducts do productController.js"})
+    }
+}
+
+//////////
+/////POST
+//////////
+
+//criar um novo produto na base de dados
+export const createProduct = async (req,res) => {
+    const {NAME,PRICE,IMAGE} = req.body;
+
+    try{
+        if(!NAME || !PRICE || !IMAGE){
+            res.status(400).json({message: "Dados formatados incorretamente, por favor verifique se falta algo e re-envie"})
+        }else{
+            const RESULT = await pool.query("INSERT INTO products (name, price, image) VALUES ($1,$2,$3) RETURNING *", [NAME,PRICE,IMAGE]);
+            res.status(201).json(RESULT.rows[0]);
+        }
+    }catch (err){
+        console.error("Erro detetado no createProduct do productController.js: ",err);
+        //Enviar codigo de erro interno de servidor 
+        res.status(500).json({message: "Erro detetado no createProduct do productController.js"})
+    }
+}
+
+/////////
+/////PUT
+/////////
+
+//atualizar um produto com os dados novos pelo ID
+export const updateProduct = async (req,res) => {
+    const {NAME,PRICE,IMAGE} = req.body;
+    const {ID} = req.params;
+
+    try{
+        if(!NAME || !PRICE || !IMAGE || !ID){
+            res.status(400).json({message: "Dados formatados incorretamente, por favor verifique se falta algo e re-envie"})
+        }else{
+            const RESULT = await pool.query("UPDATE products SET name=$1, price=$2, image=$3 WHERE id=$4 RETURNING *", [NAME,PRICE,IMAGE,ID]);
+            res.status(201).json(RESULT.rows[0]);
+        }
+    }catch (err){
+        console.error("Erro detetado no updateProduct do productController.js: ",err);
+        //Enviar codigo de erro interno de servidor 
+        res.status(500).json({message: "Erro detetado no updateProduct do productController.js"})
+    }
+}
+
+////////////
+/////DELETE
+////////////
+
+//apagar um produto atraves do seu ID
+export const deleteProduct = async (req,res) => {
+    const {ID} = req.params;
+
+    try{
+        if(!ID){
+            res.status(400).json({message: "ID não existente, por favor tente outro"})
+        }else{
+            const RESULT = await pool.query("DELETE FROM products WHERE id=$1 RETURNING *", [ID]);
+            res.status(200).json(RESULT.rows[0]);
+        }
     }catch (err){
         console.error("Erro detetado no createProduct do productController.js: ",err);
         //Enviar codigo de erro interno de servidor 
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,19 @@
 import express from "express";
-import { createProduct, getAllProducts } from "../controllers/productController.js";
+import { createProduct, getAllProducts, getProduct, updateProduct, deleteProduct } from "../controllers/productController.js";
 
 const router = express.Router();
 
+//GET
 router.get("/", getAllProducts)
+router.get("/:ID", getProduct)
 
+//POST
 router.post("/", createProduct)
+
+//PUT
+router.put("/:ID", updateProduct)
+
+//DELETE
+router.delete("/:ID", deleteProduct)
 
 export default router;
