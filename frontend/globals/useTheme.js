d4460b90bd205175376338f5ecd0a41b5e2ffa51
wolfmanPires <81977731+wolfmanPires@users.noreply.github.com>
2026-07-22 23:38:58 +0100

    Render v7

--- frontend/globals/useTheme.js
+++ frontend/globals/useTheme.js
@@ -2,7 +2,7 @@ import {create} from "zustand";
 
 //Em vez de importar a informacao do tema corrente de componente para componente, esta variavel fica guardada de forma global e utilizavel por qualquer componente (melhora escalabilidade)
 export const useTheme = create((set) => ({
-    theme: localStorage.getItem("preferred-theme") || "coffee",
+    theme: localStorage.getItem("preferred-theme") || "ASAS",
     setTheme: (theme) => {
         {/* localStorage funciona como cookies, podem ser acedidas numa altura posterior, mas nao sao muito seguras */}
         localStorage.setItem("preferred-theme", theme);
