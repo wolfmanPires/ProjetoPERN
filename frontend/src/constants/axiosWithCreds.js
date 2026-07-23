e9e652324f46c9ac53f2f7cba73865cae8fe236a
wolfmanPires <81977731+wolfmanPires@users.noreply.github.com>
2026-07-23 09:27:54 +0100

    corr render v9

--- frontend/src/constants/axiosWithCreds.js
+++ frontend/src/constants/axiosWithCreds.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const apiWithCreds = axios.create({
-    baseURL: "http://localhost:3001",
+    baseURL: "import.meta.env.VITE_API_URL",
     withCredentials: true
 });
 
